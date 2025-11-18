0a956afa133033adfada5511b2f8c60a18aa173a
Reinner Steven Daza Leiva <reinner.leiva@outlook.com>
2025-11-18 18:13:22 -0500

    Initial Commit

---
+++ RideFuture/RideFutureApp.swift
@@ -0,0 +1,17 @@
+//
+//  RideFutureApp.swift
+//  RideFuture
+//
+//  Created by Reinner Steven Daza Leiva on 18/11/25.
+//
+
+import SwiftUI
+
+@main
+struct RideFutureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
